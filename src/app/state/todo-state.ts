b2bb0cf79a0d3dfb1447d84f7a87fa6bd916a25f
Akshay Sharma <akshaya.sharma770@gmail.com>
2020-09-07 14:21:47 +0530

    To do App in angular with NGXS

---
+++ src/app/state/todo-state.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { State, NgxsOnInit, Action, StateContext } from '@ngxs/store';
+import { TodoActions } from './todo-actions';
+import { patch, insertItem, iif, updateItem } from '@ngxs/store/operators';
+
+export interface ITodo {
+    id: string;
+    title: string;
+    is_done: boolean;
+}
+
+export interface ITodoStateModel {
+    todoList: ITodo[];
+}
+
+@State<ITodoStateModel>({
+    name: 'todoList',
+    defaults: {
+        todoList: [],
+    },
+})
+@Injectable()
+export class TodoState implements NgxsOnInit {
+    ngxsOnInit(ctx) {
+        ctx.dispatch(new TodoActions.FetchAllTodos());
+    }
+
+    // we don't need the id to update, we get back the user id ( no null check, we are logged in )
+    @Action(TodoActions.markDone)
+    markDone(ctx: StateContext<ITodoStateModel>, { payload, is_done }: TodoActions.markDone) {
+
+        ctx.setState(
+            patch({
+                todoList: updateItem((item: ITodo) => item.id === payload, patch({ is_done: !is_done }))
+            })
+        );
+    }
+
+
+    @Action(TodoActions.AddTodo)
+    add(
+        ctx: StateContext<ITodoStateModel>,
+        { payload }: TodoActions.AddTodo,
+    ) {
+
+        const state = ctx.getState();
+
+        ctx.setState({
+            ...state,
+            todoList: [
+                ...state.todoList,
+                {
+                    ...payload,
+                    id: Math.random().toString(36).substring(7),
+                    is_done: false
+                }
+            ],
+        }
+        );
+    }
+
+}
